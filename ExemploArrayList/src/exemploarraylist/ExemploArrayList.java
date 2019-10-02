b9efc1d8141494f8b7ebb8f9e4a3860943feeef2
Rafael Bueno <50866088+rafadrums@users.noreply.github.com>
2019-10-01 21:46:37 -0300

    DONE

--- ExemploArrayList/src/exemploarraylist/ExemploArrayList.java
+++ ExemploArrayList/src/exemploarraylist/ExemploArrayList.java
@@ -5,6 +5,8 @@
  */
 package exemploarraylist;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author usuario
@@ -16,6 +18,40 @@ public class ExemploArrayList {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList <String> trap = new ArrayList<String>();
+  
+        
+        trap.add("Rafael");
+        trap.add("Zack");
+        trap.add("Tiago");
+        trap.add("Bianca");
+        
+        //LISTAR
+        for(int i=0; i < trap.size(); i++){
+            System.out.println(trap.get(i));
+        }
+        
+        //REMOVER
+        trap.remove(1); //indice
+        trap.remove("Tiago"); //elemento
+        
+        System.out.println("");
+       
+        //LISTAR
+        for(int i=0; i < trap.size(); i++){
+            System.out.println(trap.get(i));
+        }
+        
+        //SUBSTUIR
+        int idx = trap.indexOf("Bianca");
+        trap.set(idx, "NOTHING");
+        
+        System.out.println("");
+        
+        //LISTAR
+        for(int i=0; i < trap.size(); i++){
+            System.out.println(trap.get(i));
+        }
     }
     
 }
